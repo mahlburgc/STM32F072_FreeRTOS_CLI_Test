017c71483dd3b007cff811898dd4d777ff87bc1f
cma <cmahlburg@posteo.de>
2021-10-01 01:14:24 +0200

    inital commit, just for testing freertos cli
    Signed-off-by: cma <cmahlburg@posteo.de>

---
+++ Core/Inc/cliTask.h
@@ -0,0 +1,14 @@
+/*
+ * cliTask.h
+ *
+ *  Created on: Sep 30, 2021
+ *      Author: Christian
+ */
+
+#ifndef INC_CLITASK_H_
+#define INC_CLITASK_H_
+
+void StartCliTask(void *argument);
+
+
+#endif /* INC_CLITASK_H_ */
